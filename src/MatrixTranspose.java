573ae7129b1bd2384ab142be84c509b09c4b4989
elena-elsa101 <elenaelsacherian@gmail.com>
2026-03-28 22:54:23 +0530

    Update MatrixTranspose.java

--- src/MatrixTranspose.java
+++ src/MatrixTranspose.java
@@ -1,26 +1,23 @@
-  import java.util.*;
+ import java.util.*;
 public class MatrixTranspose {
 public static void main(String[] args){
 Scanner sc=new Scanner(System.in);
 int n=sc.nextInt();
 int[][] a=new int[n][n];
-for(int i=0;i<n;i++){
-for(int j=0;j<n;j++){
+for(int i=0;i<n;i++)
+for(int j=0;j<n;j++)
 a[i][j]=sc.nextInt();
-}
-}
-for(int i=0;i<n;i++){
+for(int i=0;i<n;i++)
 for(int j=i+1;j<n;j++){
 int t=a[i][j];
 a[i][j]=a[j][i];
 a[j][i]=t;
 }
-}
 for(int i=0;i<n;i++){
-for(int j=0;j<n;j++){
+for(int j=0;j<n;j++)
 System.out.print(a[i][j]+" ");
-}
 System.out.println();
 }
 }
 }
+
